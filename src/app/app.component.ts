35eedafc0980d1956ba4111e44b31e850e7a4c30
Daniel Sogl <daniel.sogl@thinktecture.com>
2024-08-15 21:31:47 +0200

    reproduce bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  ButtonModule,
+  ComboBoxModule,
+  ListItem,
+} from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, ComboBoxModule, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'combo-box-bug';
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  protected readonly listItems = signal<ListItem[]>([]);
+
+  protected readonly form = new FormGroup({
+    comboBox: new FormControl(1),
+  });
+
+  protected lazyLoadItems() {
+    this.listItems.set([
+      { content: 'Item 1', value: 1, selected: false },
+      { content: 'Item 2', value: 2, selected: false },
+      { content: 'Item 3', value: 3, selected: false },
+    ]);
+    this.cdr.detectChanges();
+  }
 }
